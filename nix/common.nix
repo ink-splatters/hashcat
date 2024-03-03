2ddaf19ff5b9182b498781f38f8be4a1b0c56ccc
Peter A. <ink.splatters@pm.me>
2024-03-03 01:27:12 +0100

    improvements

--- nix/common.nix
+++ nix/common.nix
@@ -1,13 +1,14 @@
-{ pkgs, system, ... }:
-let inherit (pkgs.darwin.apple_sdk) frameworks;
-
-in with pkgs; rec {
+{ darwin, lib, lld_17, llvmPackages_17, gnumake, iconv, system, ... }:
+let
+  inherit (darwin.apple_sdk) frameworks;
+  inherit (llvmPackages_17) libcxx libcxxabi bintools;
+in rec {
   CFLAGS = lib.optionalString ("${system}" == "aarch64-darwin")
     "-mcpu=apple-m1 -fuse-ld=lld -flto=full";
   CXXFLAGS = "${CFLAGS}";
   LDFLAGS = "-fuse-ld=lld -flto=full";
 
-  nativeBuildInputs = [ lld_17 clang-tools_17 gnumake llvm_17 ];
+  nativeBuildInputs = [ lld_17 gnumake bintools ];
   buildInputs = with frameworks;
     [ OpenCL IOKit Foundation Metal ] ++ [ iconv libcxx libcxxabi ];
 }

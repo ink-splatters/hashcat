ec179139a976c5b704e02396cd7c0ac744433e97
Peter A. <ink.splatters@pm.me>
2024-03-03 02:49:43 +0100

    aarch64-darwin cache

--- flake.nix
+++ flake.nix
@@ -19,9 +19,10 @@
   };
 
   nixConfig = {
-    extra-substituters = "https://cachix.cachix.org";
+    extra-substituters =
+      "https://cachix.cachix.org https://aarch64-darwin.cachix.org ";
     extra-trusted-public-keys =
-      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM=";
+      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM= aarch64-darwin.cachix.org-1:mEz8A1jcJveehs/ZbZUEjXZ65Aukk9bg2kmb0zL9XDA=";
   };
 
   outputs = { nixpkgs, flake-utils, pre-commit-hooks, self, ... }:
